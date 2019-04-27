9da1dfc02a309bdf2ea148f6274a339800646dcd
Viktor Abrahamsson <wicke.abrahamsson@gmail.com>
2019-04-28 00:08:42 +0200

    Datepicker, form-rules and more

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -6,9 +6,18 @@
                 <h2>Add a New Project</h2>
             </v-card-title>
             <v-card-text>
-                <v-form class="px-3">
-                    <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
-                    <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+                <v-form class="px-3" ref="form">
+                    <v-text-field label="Title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+                    <v-textarea label="Information" v-model="content" prepend-icon="edit" :rules="inputRules"></v-textarea>
+
+                    <v-menu>
+                        <v-text-field :value="formattedDate" slot="activator" lable="Due Date" prepend-icon="date_range"
+                        :rules="inputRules"></v-text-field>
+                        v-<v-date-picker v-model="due"></v-date-picker>
+                    </v-menu>
+
+                    <v-spacer></v-spacer>
+
                     <v-btn flat class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
                 </v-form>
             </v-card-text>
@@ -18,18 +27,33 @@
 </template>
 
 <script>
+import format from 'date-fns/format'
+
 export default {
     data() {
         return {
             title: '',
-            content: ''
+            content: '',
+            due: null,
+            inputRules: [ // v står för value.
+                v => v.length >= 3 || 'Minimum length is 3 characters'
+                // Detta är bara för att lära mig, men i framtiden bör specifika
+                // meddelanden och regler vara för olika element.
+            ]
         }
     },
     methods: {
         submit() {
-            console.log(this.title, this.content)
+            if (this.$refs.form.validate()) {
+                console.log(this.title, this.content)
+            }    
         }
-    }
+    },
+    computed: {
+        formattedDate() {
+            return this.due ? format(this.due, 'Do MMM YYYY') : ''
+        }
+    },
 }
 </script>
 
